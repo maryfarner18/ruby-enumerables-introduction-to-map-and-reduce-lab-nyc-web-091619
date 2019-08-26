ff6f167b570aa01aa4f30c0e55d33f8739e5f73c
Mary Farner <maryfarner@gmail.com>
2019-08-26 01:03:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -63,5 +63,11 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  
+  i = 0
+  while i < source_array.length do
+    if source_array[i] == false
+      return FALSE
+    end
+  end
+  TRUE
 end
