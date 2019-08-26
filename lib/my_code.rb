f54d66f9e723a351e3e9954cb5d70484b8fac784
Mary Farner <maryfarner@gmail.com>
2019-08-26 01:02:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,7 +53,13 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  
+   sum = 0
+  i = starting_point 
+  while i < source_array.length do
+    sum += source_array[i]
+    i += 1 
+  end
+  sum
 end
 
 def reduce_to_any_true(source_array)
